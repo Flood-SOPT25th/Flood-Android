33e1aa5e6fb1100ee579cbe40e650e51e51dc67e
JiheeSeon <starlover27@naver.com>
2019-12-27 01:30:07 +0900

    회원가입, 마이페이지, 알림 UI 구현

---
+++ app/src/main/java/com/flood_android/network/Jihee/MypageMyflipRvItem.kt
@@ -0,0 +1,7 @@
+package com.flood_android.network.Jihee
+
+data class MypageMyflipRvItem(
+    val thumnail : String,
+    val category_name : String,
+    val flipnum : Int
+)
--- app/src/main/java/com/flood_android/ui/signup/SignupActivity.kt
+++ app/src/main/java/com/flood_android/ui/signup/SignupActivity.kt
@@ -5,10 +5,11 @@ import android.os.Bundle
 import com.flood_android.R
 
 class SignupActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(android.R.style.Theme_NoTitleBar_Fullscreen)
         setContentView(R.layout.activity_signup)
     }
+
+
 }
