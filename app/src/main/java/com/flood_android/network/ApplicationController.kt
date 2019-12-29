9ce34e4daa6bd674a98fb6a8e0b987cd239b9833
JiheeSeon <starlover27@naver.com>
2019-12-29 20:03:20 +0900

    지희, signup 3-5 구현중

--- app/src/main/java/com/flood_android/network/ApplicationController.kt
+++ app/src/main/java/com/flood_android/network/ApplicationController.kt
@@ -4,30 +4,21 @@ import android.app.Application
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class ApplicationController: Application() {
+object ApplicationController {
+    private val baseURL = "http://flooddocker-env.3gfczrrijh.ap-northeast-2.elasticbeanstalk.com"
+
     lateinit var networkServiceFeed: NetworkServiceFeed
     lateinit var networkServiceUser: NetworkServiceUser
 
-    private val baseUrl = ""
-
-    companion object {
-        lateinit var instance : ApplicationController
-    }
-
-
-    override fun onCreate() {
-        super.onCreate()
-        instance = this
+    init{
         buildNetwork()
     }
 
     fun buildNetwork(){
-
-        val builder = Retrofit.Builder()
-        val retrofit = builder
-            .baseUrl(baseUrl)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+    val retrofit : Retrofit = Retrofit.Builder()
+        .baseUrl(baseURL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 
         networkServiceFeed = retrofit.create(NetworkServiceFeed::class.java)
         networkServiceUser = retrofit.create(NetworkServiceUser::class.java)
--- app/src/main/java/com/flood_android/network/NetworkServiceFeed.kt
+++ app/src/main/java/com/flood_android/network/NetworkServiceFeed.kt
@@ -1,25 +1,48 @@
 package com.flood_android.network
 
 import com.flood_android.ui.feed.data.GetFeedCategoryResponse
+import com.flood_android.ui.feed.data.GetFeedTop3Response
+import com.flood_android.ui.feed.data.PostBookmarkCancelResponse
+import com.flood_android.ui.post.PostPostResponse
+import com.flood_android.ui.post.get.GetPostResponse
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.*
 
 interface NetworkServiceFeed {
+    // 해당 조직의 카테고리
     @GET("/group/category")
     fun getFeedCategoryResponse(
+        @Header("Authorization") autrhorization : String
     ): Call<GetFeedCategoryResponse>
-/*
+
+    // Top3 피드 조회
+    @GET("/post/top")
+    fun getFeedTop3Response(
+        @Header("Authorization") authorization: String
+    ): Call<GetFeedTop3Response>
+
+    /**
+     * 게시물 등록 POST
+     */
     @Multipart
     @POST("/post")
     fun postPostResponse(
-        @Header("token") token: String,
-        @Part images: MultipartBody.Part?,
+        @Header("Authorization") token: String,
+        @Part images: ArrayList<MultipartBody.Part>?,
         @Part("url") url: RequestBody,
         @Part("category") category: RequestBody,
-        @Part("content") content: RequestBody
+        @Part("postContent") content: RequestBody
     ): Call<PostPostResponse>
 
- */
+    /**
+     * 게시물 등록 GET
+     */
+    @GET("/group/category")
+    fun getPostResponse(
+        @Header("Authorization") token: String
+    ): Call<GetPostResponse>
 }
