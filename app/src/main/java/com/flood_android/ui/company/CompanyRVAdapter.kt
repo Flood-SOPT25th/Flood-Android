7bfb68cd4e6a4054b89a2586c884f9dd27018237
cheongha <sch7188@gmail.com>
2020-01-02 18:54:20 +0900

    Company Activity UI, 기능, 서버 구현

---
+++ app/src/main/java/com/flood_android/ui/company/CompanyRVAdapter.kt
@@ -0,0 +1,67 @@
+package com.flood_android.ui.company
+
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat.startActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import com.flood_android.R
+import com.flood_android.ui.main.MainActivity
+import com.flood_android.util.OnSingleClickListener
+
+class CompanyRVAdapter(
+    val ctx: Context,
+    var dataList: List<GroupArr>
+) : RecyclerView.Adapter<CompanyRVAdapter.Holder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.rv_item_company, parent, false)
+
+        return Holder(view)
+    }
+
+    override fun getItemCount(): Int = dataList.size
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        var options: RequestOptions = RequestOptions().transform(CenterCrop(), RoundedCorners(50))
+
+
+        Log.v("postygyg", "postygyg")
+        Glide.with(ctx)
+            .load(dataList[position].groupImage)
+            .apply(options)
+            .into(holder.companyImage)
+        holder.companyName.text = dataList[position].name.trim()
+        Log.v("postygyg", dataList[position].category.toString())
+        holder.category1.text = dataList[position].category[1]
+        holder.category2.text = dataList[position].category[2].trim()
+        holder.category3.text = dataList[position].category[3].trim()
+
+        holder.companyImage.setOnClickListener {
+            (ctx as MainActivity).detailset(dataList[position].groupCode)
+        }
+
+    }
+
+    inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val companyContainer: ConstraintLayout = itemView.findViewById(R.id.cl_company)
+        val companyImage: ImageView = itemView.findViewById(R.id.iv_rv_item_company_img)
+        val companyName: TextView = itemView.findViewById(R.id.tv_rv_item_company_name)
+        val category1: TextView = itemView.findViewById(R.id.tv_rv_item_company_category1)
+        val category2: TextView = itemView.findViewById(R.id.tv_rv_item_company_category2)
+        val category3: TextView = itemView.findViewById(R.id.tv_rv_item_company_category3)
+
+    }
+
+}
