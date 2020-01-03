1c857d555bb14845dc79707add1131c5a2ef82cb
cheongha <sch7188@gmail.com>
2020-01-03 17:16:06 +0900

    MyPageEdit 뷰 UI, 기능 구현

---
+++ app/src/main/java/com/flood_android/ui/myprofile/PasswordSetActivity.kt
@@ -0,0 +1,13 @@
+package com.flood_android.ui.myprofile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.flood_android.R
+
+class PasswordSetActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_password_set)
+    }
+}
